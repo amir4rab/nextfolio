46186aab242344b7d93ce1792b176a2ddfa4a330
amir4rab <60965542+amir4rab@users.noreply.github.com>
2022-08-25 05:34:53 +0000

    icons row math based animations added

--- src/subcomponents/iconsRow.tsx
+++ src/subcomponents/iconsRow.tsx
@@ -1,7 +1,10 @@
-import { useMediaQuery } from '@mantine/hooks';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+// react-icons
+import type { IconType } from 'react-icons';
+
+// mantine
 import { createStyles, keyframes } from '@mantine/styles';
-import { useState } from 'react';
-import { IconType } from 'react-icons';
 
 export interface IconsRowProps {
   icons: {
@@ -13,11 +16,10 @@ export interface IconsRowProps {
 // keyframes
 const slidingAnimation = keyframes({
   '0%': {
-    // transform: 'translate(-2rem, -50%)'
     left: '-2.5rem'
   },
   '100%': {
-    left: 'calc( 100% + 2.5rem)'
+    left: 'calc( 100% + 2.5vw)'
   }
 });
 
@@ -52,12 +54,14 @@ const useStyles = createStyles((t) => ({
   },
   icon: {
     left: '-2.5rem',
+    width: '2.5rem',
+    height: '2.5rem',
     top: '50%',
     position: 'absolute',
     color: t.colorScheme === 'dark' ? t.white : t.black,
     opacity: 0.5,
     fontSize: '2rem',
-    transition: 'opacity .3s ease-in-out',
+    transition: 'opacity .3s ease-in-out, color .15s ease-in-out',
     transform: 'translate(0, -50%)',
     display: 'flex',
     justifyContent: 'center',
@@ -66,11 +70,12 @@ const useStyles = createStyles((t) => ({
       marginRight: '.25rem'
     },
     ['&:hover']: {
-      opacity: 1
+      opacity: 1,
+      color: t.primaryColor
     },
     animation: `${slidingAnimation} linear forwards infinite`,
     [t.fn.largerThan('md')]: {
-      animationDuration: '22s'
+      animationDuration: '20s'
     },
     [t.fn.smallerThan('md')]: {
       animationDuration: '10s'
@@ -81,35 +86,78 @@ const useStyles = createStyles((t) => ({
   }
 }));
 
+/** Calculates amount of delay needed for equal padding between items */
+const getTiming = (
+  arrayLength: number,
+  wrapperWidth: number,
+  isDesktop: boolean
+) => {
+  const animationDuration = isDesktop ? 20 : 10;
+  const elementWidth = 40; // 2.5rem * 16 = 40px
+  const wrapperWidthWithPadding = wrapperWidth + 80;
+  const speed = wrapperWidthWithPadding / animationDuration; // path / duration of path
+
+  const emptySpace = wrapperWidthWithPadding - elementWidth * arrayLength;
+  const emptySpacePerItem = emptySpace / arrayLength;
+
+  const durationNeededForPadding = (emptySpacePerItem + elementWidth) / speed;
+
+  return parseFloat(durationNeededForPadding.toFixed(2));
+};
+
 const IconsRow = ({ icons }: IconsRowProps) => {
   const { classes, cx } = useStyles();
   const [hovered, setHovered] = useState(false);
-  const isDesktop = useMediaQuery('(min-width: 966px)', false);
+  const [delayLength, setDelayLength] = useState<null | number>(null);
+  const wrapperRef = useRef<HTMLDivElement | null>(null);
+
+  const animationCalculations = useCallback(() => {
+    if (wrapperRef.current === null) return;
+
+    const isDesktop = window.matchMedia('(min-width: 966px)').matches;
+    const width = wrapperRef.current.getBoundingClientRect().width;
+
+    const delay = getTiming(icons.length, width, isDesktop);
+
+    setDelayLength(delay);
+  }, [icons.length]);
+
+  useEffect(() => {
+    if (wrapperRef.current === null || typeof window === 'undefined') return;
+
+    delayLength === null && animationCalculations();
+
+    window.addEventListener('resize', animationCalculations);
+
+    return () => {
+      window.removeEventListener('resize', animationCalculations);
+    };
+  }, [animationCalculations, delayLength]);
 
   return (
-    <div className={classes.iconsRow}>
+    <div ref={wrapperRef} className={classes.iconsRow}>
       <p className={classes.title}>Experienced with:</p>
       <div
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
         className={classes.iconsWrapper}>
-        {icons.map(({ icon, href }, i) => {
-          const I = icon;
-          return (
-            <a
-              href={href}
-              target='_blank'
-              rel='noreferrer'
-              key={i}
-              className={cx(classes.icon, hovered && classes.iconPaused)}
-              style={{
-                // animationDelay: i * 2.75 + 's'
-                animationDelay: i * (isDesktop ? 2.75 : 1.25) + 's'
-              }}>
-              <I />
-            </a>
-          );
-        })}
+        {delayLength !== null &&
+          icons.map(({ icon, href }, i) => {
+            const I = icon;
+            return (
+              <a
+                href={href}
+                target='_blank'
+                rel='noreferrer'
+                key={i}
+                className={cx(classes.icon, hovered && classes.iconPaused)}
+                style={{
+                  animationDelay: i * delayLength + 's'
+                }}>
+                <I />
+              </a>
+            );
+          })}
       </div>
     </div>
   );
