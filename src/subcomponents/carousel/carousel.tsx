f000cdc0ca5d3755b3d0842fdae8ee1cbb7a8a93
amir4rab <60965542+amir4rab@users.noreply.github.com>
2023-03-15 06:37:34 +0100

    updated Carousel component

--- src/subcomponents/carousel/carousel.tsx
+++ src/subcomponents/carousel/carousel.tsx
@@ -9,11 +9,11 @@ import { motion, AnimatePresence } from 'framer-motion';
 // icons
 import { IoChevronBack, IoChevronForward } from 'react-icons/io5';
 
-// mantine
-import { createStyles } from '@mantine/styles';
+// styles
+import classes from './carousel.module.scss';
 
 // components
-import Button from './button';
+import Button from '../button';
 
 const itemVariants: AnimationProps['variants'] = {
   display: {
@@ -30,61 +30,6 @@ const itemVariants: AnimationProps['variants'] = {
   }
 };
 
-// styles
-const useStyle = createStyles((t) => ({
-  carousel: {
-    position: 'relative',
-    width: '100%',
-    height: '40vh'
-  },
-  imageWrapper: {
-    position: 'absolute',
-    left: '50%',
-    top: '0%',
-    height: '100%',
-    width: '100%',
-    overflow: 'hidden',
-    display: 'flex',
-    justifyContent: 'center',
-    alignContents: 'center',
-    alignItems: 'center',
-    pointerEvents: 'none'
-  },
-  image: {
-    objectFit: 'contain',
-    width: '100%',
-    maxHeight: '100%'
-  },
-  button: {
-    zIndex: 5,
-    display: 'flex',
-    justifyItems: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    top: '50%',
-    transform: 'translate(0, -50%)',
-    padding: t.spacing.xs,
-    borderRadius: '50%',
-    ['&:hover']: {
-      transform: 'translate(0, calc(-50% - .05rem))',
-      background: t.colors.dark[4]
-    },
-    ['&:active']: {
-      transform: 'translate(0, calc(-50% - .1rem))',
-      background: t.colors.dark[5]
-    },
-    ['&:disabled']: {
-      opacity: 0
-    }
-  },
-  rightButton: {
-    right: '1rem'
-  },
-  leftButton: {
-    left: '1rem'
-  }
-}));
-
 interface Props {
   images: string[];
   ratio: string;
@@ -92,7 +37,6 @@ interface Props {
 
 const Carousel = ({ images, ratio }: Props) => {
   const [activeItem, setActiveItem] = useState(0);
-  const { classes, cx } = useStyle();
 
   return (
     <motion.div
@@ -102,13 +46,13 @@ const Carousel = ({ images, ratio }: Props) => {
       <Button
         disabled={activeItem <= 0}
         onClick={() => setActiveItem((curr) => curr - 1)}
-        className={cx(classes.button, classes.leftButton)}>
+        className={[classes.button, classes.leftButton].join(' ')}>
         <IoChevronBack />
       </Button>
       <Button
         disabled={activeItem >= images.length - 1}
         onClick={() => setActiveItem((curr) => curr + 1)}
-        className={cx(classes.button, classes.rightButton)}>
+        className={[classes.button, classes.rightButton].join(' ')}>
         <IoChevronForward />
       </Button>
       <AnimatePresence>
