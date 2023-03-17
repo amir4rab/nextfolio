44fa0e48d478f652f6b01b8a798f70a7bbfacd7c
amir4rab <60965542+amir4rab@users.noreply.github.com>
2023-03-17 16:09:57 +0100

    added toggle subcomponent

---
+++ src/subcomponents/toggle/toggle.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import classes from './styles.module.scss';
+
+interface Props {
+  onChange?: (v: boolean) => void;
+  defaultChecked?: boolean;
+  label?: string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+}
+
+const genRandomName = () => (Math.random() * 1_000_000).toFixed(0);
+
+const Toggle = ({
+  onChange,
+  defaultChecked = false,
+  size = 'md',
+  label = ''
+}: Props) => {
+  const [localId, setLocalId] = useState<undefined | string>();
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    setLocalId(label + '-' + genRandomName());
+  }, [label]);
+
+  return (
+    <div className={classes.toggle}>
+      <input
+        id={localId}
+        defaultChecked={defaultChecked}
+        type='checkbox'
+        onChange={(e) => onChange && onChange(e.target.checked)}
+      />
+      <label
+        htmlFor={localId}
+        style={{
+          height: `calc(var(--spacing-${size}) * 1.25)`,
+          width: `calc(var(--spacing-${size}) * 2.5)`
+        }}>
+        {label}
+      </label>
+    </div>
+  );
+};
+
+export default Toggle;
