5d0580d61d2da811fed5da6e0e93b9fcdce3b8ff
amir4rab <60965542+amir4rab@users.noreply.github.com>
2023-03-18 09:13:19 +0100

    updated layout

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,79 +1,60 @@
-import { Suspense } from 'react';
+'use client';
+
 import type { ReactNode } from 'react';
 
-// next
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
+// styles
+import classes from './styles.module.scss';
 
-// mantine
-import { createStyles } from '@mantine/styles';
-
-// components
+// subcomponents
 import DesktopNavbar from './desktopNavbar';
-import MobileNavbar from './mobileNavbar';
+import MobileNavbar from './mobileNavbar/mobileNavbar';
 
-const AuroraBackground = dynamic(() => import('./auroraBackground'), {
-  suspense: true
-});
-const Footer = dynamic(() => import('./footer'), {
-  suspense: true
-});
-
-// constants
-const desktopMaxWidth = 966;
-
-const useStyles = createStyles((t) => ({
-  main: {
-    padding: '5vh 0',
-    [t.fn.largerThan('md')]: {
-      margin: '0 auto',
-      minHeight: '90.675vh',
-      maxWidth: desktopMaxWidth
-    },
-    [t.fn.smallerThan('md')]: {
-      minHeight: '94vh',
-      padding: `0 ${t.spacing.md}px ${t.spacing.md}px ${t.spacing.md}px`
-    }
-  }
-}));
+// icons
+import {
+  IoFolderOpen,
+  IoHome,
+  IoInformationCircle,
+  IoSettings
+} from 'react-icons/io5';
 
 interface Props {
   children: ReactNode;
 }
 
 const Layout = ({ children }: Props) => {
-  const { classes } = useStyles();
-  const { pathname } = useRouter();
-
   return (
     <>
       <DesktopNavbar
-        desktopMaxWidth={desktopMaxWidth}
         socials={{
           github: 'https://github.com/amir4rab',
           codeSandbox: 'https://codesandbox.io/u/amir4rab',
-          npm: 'https://www.npmjs.com/~amir4rab'
+          linkedin: 'https://www.linkedin.com/in/amir4rab/'
         }}>
-        <DesktopNavbar.Item href='/'>Home</DesktopNavbar.Item>
-        <DesktopNavbar.Item href='/about'>About</DesktopNavbar.Item>
-        <DesktopNavbar.Item
-          active={pathname.includes('showcase')}
-          href='/#showcase'>
-          Showcase
-        </DesktopNavbar.Item>
-        <DesktopNavbar.Item href='/projects'>Projects</DesktopNavbar.Item>
-        <DesktopNavbar.Item href='/blog'>Blog</DesktopNavbar.Item>
+        <DesktopNavbar.Item href='/' title='Home' />
+        <DesktopNavbar.Item href='/about' title='About' />
+        <DesktopNavbar.Item href='/projects' title='Projects' />
       </DesktopNavbar>
-      <MobileNavbar />
-      <Suspense fallback={null}>
-        <AuroraBackground />
-      </Suspense>
-      <main id='__main' className={classes.main}>
+      <main style={{ height: '300vh' }} id='__main' className={classes.main}>
         {children}
       </main>
-      <Suspense fallback={null}>
-        <Footer desktopMaxWidth={desktopMaxWidth} />
-      </Suspense>
+      <MobileNavbar>
+        <MobileNavbar.Item href='/' title='Home' icon={<IoHome />} />
+        <MobileNavbar.Item
+          href='/about'
+          title='About'
+          icon={<IoInformationCircle />}
+        />
+        <MobileNavbar.Item
+          href='/projects'
+          title='Projects'
+          icon={<IoFolderOpen />}
+        />
+        <MobileNavbar.Item
+          href='/settings'
+          title='Settings'
+          icon={<IoSettings />}
+        />
+      </MobileNavbar>
     </>
   );
 };
